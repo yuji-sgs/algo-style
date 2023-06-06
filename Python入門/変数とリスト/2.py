30ba53b90d5fa66672ea26e1233942a3fbbd58c0
Yuji Sato <60169552+yuji-sgs@users.noreply.github.com>
2023-06-06 17:01:49 +0900

    Create 2.py

---
+++ Python入門/変数とリスト/2.py
@@ -0,0 +1,6 @@
+"""
+エディタに書かれているコードは、変数 name に aruru という名前を代入したものです。
+変数 name を適切に利用し、次の自己紹介を出力するプログラムを作成してください。
+"""
+name = "aruru" # 変数 name に "aruru" を代入
+print("My name is {}!".format(name)) # name を用いた自己紹介を表示
