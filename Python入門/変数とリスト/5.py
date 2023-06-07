4345c3678ab0be2db23fa8f8bf51cd1a832725e0
Yuji Sato <60169552+yuji-sgs@users.noreply.github.com>
2023-06-07 18:48:11 +0900

    Create 5.py

---
+++ Python入門/変数とリスト/5.py
@@ -0,0 +1,11 @@
+A = ["Hello", "Algo"]
+B = ["SUN", "MON", "TUE", "WED", "TSU", "FRI", "SAT"]
+C = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+
+A.append("Method")
+print(A)
+
+print(len(B))
+
+C.sort()
+print(C)
