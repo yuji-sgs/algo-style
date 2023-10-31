3360a178294a33f9d9208ebec8bc447882d6dbee
Yuji Sato <60169552+yuji-sgs@users.noreply.github.com>
2023-10-31 14:28:24 +0900

    Create 4.sql

---
+++ データベース入門 (1)/データの集計と編集/4.sql
@@ -0,0 +1,13 @@
+insert into
+    prefectures
+values
+    (36, '徳島県', 4146, 720000),
+    (37, '香川県', 1876, 950000),
+    (38, '愛媛県', 5675, 1335000),
+    (39, '高知県', 7102, 692000);
+select
+    *
+from
+    prefectures
+order by
+    id
